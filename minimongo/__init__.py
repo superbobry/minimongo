61324248acbc8748d71646c2af0143ca45117a73
Sergei Lebedev <superbobry@gmail.com>
2011-06-08 14:30:27 +0400

    connect() now takes `lazy` argument -- so we can force connection if needed

--- minimongo/__init__.py
+++ minimongo/__init__.py
@@ -16,15 +16,17 @@ from .model import AttrDict, Model, ModelBase
 from .meta import Rule
 
 
-def connect(database, host='localhost', port=27017):
+def connect(database, host='localhost', port=27017, lazy=True):
     """Creates a module-wide connection to MongoDB.
 
     :param unicode database: database name to connect to.
     :param unicode host: MongoDB server hostname.
     :param int port: MongoDB server port.
+    :param bool lazy: don't connect to the database, unless it's
+                      required.
     """
     from pymongo import Connection
-    ModelBase.db = Connection(host, port, _connect=False)[database]
+    ModelBase.db = Connection(host, port, _connect=not lazy)[database]
 
     # Iterate over all of the already declared models and auto index
     # them.
@@ -37,3 +39,37 @@ def connect(database, host='localhost', port=27017):
 
     return ModelBase.db
 
+
+# Monkey-patching hell! C-BSON parser doesn't support anything but
+# `dict`, so we are forced to do some dirty stuff, if we want to
+# use other Mapping suptypes.
+import bson
+import struct
+from collections import Mapping
+
+def _dict_to_bson(dict, check_keys, top_level=True):
+    try:
+        elements = b""
+        if top_level and "_id" in dict:
+            elements += bson._element_to_bson("_id", dict["_id"], False)
+        for (key, value) in dict.iteritems():
+            if not top_level or key != "_id":
+                elements += bson._element_to_bson(key, value, check_keys)
+    except AttributeError:
+        raise TypeError("encoder expected a mapping type but got: %r" % dict)
+
+    length = len(elements) + 5
+    return struct.pack(b"<i", length) + elements + b"\x00"
+
+bson._dict_to_bson = _dict_to_bson
+
+def _element_to_bson(key, value, check_keys):
+    try:
+        return _old_element_to_bson(key, value, check_keys)
+    except bson.InvalidDocument:
+        name = bson._make_c_string(key, True)
+        if isinstance(value, Mapping):
+            return b"\x03" + name + _dict_to_bson(value, check_keys, False)
+
+_old_element_to_bson = bson._element_to_bson
+bson._element_to_bson = _element_to_bson
